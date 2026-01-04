6e0e6d1747f6dec62954a3403f4e9d8b8d58fa3a
wekindos <rogashianmvungi@gmail.com>
2026-01-04 15:00:18 +0300

    PHP-form registration and login

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include_once __DIR__ . '/includes/header.php' ?>
+    <a href="./login.php" class="primary">Login</a>
+    <a href="./registration.php" class="secondary">Register</a>
+<?php include_once __DIR__ . '/includes/footer.php' ?>
